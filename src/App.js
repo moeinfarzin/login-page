3104d25d07fae484d4ce9792ca2591e0a2294a15
moeinFarzin <moein.akd12345@gmail.com>
2022-09-25 11:07:24 -0700

    deployed website

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 function App() {
   return (
     <BrowserRouter>
+
       <Routes>
         <Route path='/' element={<LoginPage />} />
       </Routes>
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -2,8 +2,12 @@ import './LoginPage.css'
 import React from 'react'
 import logo from './logo.svg'
 import { Link } from 'react-router-dom'
+
 const LoginPage = () => {
+    document.title="login";
     return (
+        <>
+
         <div className='login-page'>
             <div className='logo-container'>
                 <div className='text'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae saepe voluptatum sapiente minus suscipit atque.</p></div>
@@ -17,13 +21,13 @@ const LoginPage = () => {
                 <div className='login-form'>
                     <form>
                         <div className='username'>
-                            <input className='username-input' type="text" placeholder='username/email' />
+                            <input className='username-input' type="text" placeholder='Username/Email' />
                         </div>
                         <div className='password'>
-                            <input className='password-input' type="text" placeholder='password' />
+                            <input className='password-input' type="text" placeholder='Password' />
                         </div>
                         <div className='login-btn'>
-                            <button className='btn'>login</button>
+                            <button className='btn'>Login</button>
                         </div>
                         <div className='remember-forget'>
                             <div className='remember'>
@@ -43,6 +47,7 @@ const LoginPage = () => {
             </div>
 
         </div>
+        </>
     )
 }
 export default LoginPage
